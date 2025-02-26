17f8cf79902df07584e3950c6eacbfb475d26c58
Ömer Faruk Asil <asilo02@hotmail.com>
2025-02-26 17:58:00 +0300

    api gizlendi

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
-const API_KEY = "5b3ce3597851110001cf624855b9272ed8aa4d348fdc6a90426c7565";
+const API_KEY = "OpenRoute API KEY";
 const DEFAULT_CENTER = [32.8597, 39.9334]; // Ankara
 const DEFAULT_ZOOM = 10;
